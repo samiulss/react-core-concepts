d2a9d57fb8c23c1f4d3d1cfb024767903d892ec0
samiulss <works.samiul@gmail.com>
2022-10-26 23:58:15 +0600

    practicing

--- src/App.js
+++ src/App.js
@@ -2,28 +2,46 @@
 import './App.css';
 
 function App() {
+  const listProduct = [
+    {name:'Apple', price:'200 Taka KG'},
+    {name:'Mango', price:'150 Taka KG'},
+    {name:'Orange', price:'250 Taka KG'},
+    {name:'Banana', price:'20 Taka Hali'},
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Products listProduct = {listProduct[0]}></Products>
+        <Products listProduct = {listProduct[1]}></Products>
+        <Products listProduct = {listProduct[2]}></Products>
+        <Products listProduct = {listProduct[3]}></Products>
       </header>
     </div>
   );
 }
-function Person() {
+function Products(props) {
+  const style = {
+    border:'1px solid lime',
+    padding:'10px',
+    width:'200px',
+    height:'200px',
+    borderRadius: '8px',
+    marginBottom:'10px',
+    float:'left',
+    marginRight:'9px',
+    background:'tomato'
+  }
+  const {name, price} = props.listProduct;
   return (
-    <div style={{
-      border: '1px solid yellow',
-      margin: '10px',
-      padding: '10px',
-      borderRadius: '10px'
-      }}>
-      <h1>Name: Md Samiul</h1>
-      <h2>I'm learning React</h2>
+    <div style={style}>
+      <h4>{name}</h4>
+      <h5>{price}</h5>
+      <button id='btn' style={{
+        bacKGround:'skyblue',
+        borderRadius:'5px',
+        padding:'5px'
+        }}>Buy now</button>
     </div>
   )
 }
-
 export default App;
